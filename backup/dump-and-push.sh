ab7f93e9f8d596af6e0520321fef1e850edec444
Cédric Donner <cedonner@gmail.com>
2017-10-09 12:00:38 +0200

    petite modif pour pouvoir puller sans problème les backups

--- backup/dump-and-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /backups 
-sqlite3 /sqlite-data/data.sqlite .dump > dump.sql 
-git add dump.sql
-git commit -m "auto backup $(date +%d-%m-%Y-%H:%M)" 
-git push origin $BACKUP_BRANCH
